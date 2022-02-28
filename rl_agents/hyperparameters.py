ebba9d42117bbe5f9926734dbfc0b42e227489bf
ryan.hunt <ryan.hunt@dataspartan.com>
2022-02-28 22:10:09 +0000

    Training directories fix

--- rl_agents/hyperparameters.py
+++ rl_agents/hyperparameters.py
@@ -20,15 +20,6 @@ MAX_EVAL_EP_LENGTH = 3000
 LOG_DIR = BASE_DIR + ENV_ID + '/'
 os.makedirs(LOG_DIR, exist_ok=True)
 
-# Create a new directory for this run.
-i = 0
-while os.path.isdir(LOG_DIR + f'run_{i}/'):
-    i += 1
-LOG_DIR += f'run_{i}/'
-os.makedirs(LOG_DIR, exist_ok=True)
-
-TENSORBOARD_LOG = LOG_DIR + 'tensorboard/'
-os.makedirs(TENSORBOARD_LOG, exist_ok=True)
 TB_LOG_NAME = 'run'
 
 
